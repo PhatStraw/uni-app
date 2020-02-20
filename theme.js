5272ba3a4e54a80e58b3e3e041b86697aab96e86
kevinsims1 <kevinsims1@outlook.com>
2020-02-19 22:22:53 -0800

    90 min up

---
+++ theme.js
@@ -0,0 +1,13 @@
+import {roboto} from '@theme-ui/presets'
+import {merge} from 'theme-ui'
+
+// example theme.js
+export default merge(roboto,{
+    colors: {
+      primary: 'rgb(35,72,163)',
+      secondary: 'rgb(16,21,46)',
+      border: 'lightgrey'
+    }
+  })
+
+  
